2e8f217a368567b0b1626659cb829b0e56d9ecf9
Abdullah-Elgendy <abdullahelgendy38@gmail.com>
2025-07-25 08:19:32 +0300

    finished routing + home and about pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Contact } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'prefix' },
+  { path: 'home', component: Home, pathMatch: 'full', title: 'Home' },
+  { path: 'about', component: About, pathMatch: 'full', title: 'About' },
+  { path: 'portfolio', component: Portfolio, pathMatch: 'full', title: 'portfolio' },
+  { path: 'contact', component: Contact, pathMatch: 'full', title: 'Contact' },
+];
---
+++ src/app/components/about/about.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Title } from '../title/title'
+
+@Component({
+  selector: 'app-about',
+  imports: [Title],
+  templateUrl: './about.html',
+  styleUrl: './about.css'
+})
+export class About {
+
+}
